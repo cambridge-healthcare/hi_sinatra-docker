99d6c12f182de4b6442f42d2bddd95498f0b05f4
Gerhard Lazu <gerhard@lazu.co.uk>
2013-10-04 17:58:00 +0100

    Demonstrating redis container connectivity

--- hi.rb
+++ hi.rb
@@ -1,5 +1,19 @@
 require 'sinatra'
 
+redis_host = ENV.fetch('REDIS_HOST', false)
+if redis_host
+  require 'redis'
+  redis = Redis.new(:host => redis_host)
+end
+
+
 get '/' do
- "Hi, I am a Sinatra app running inside a docker container"
+ result = ["<p>Hi, I am a Sinatra app running inside a docker container</p>"]
+
+ if redis_host
+   hits = redis.incr("get_root_hits")
+   result << "<p>I have been displayed #{hits} times</p>"
+ end
+
+ result
 end
